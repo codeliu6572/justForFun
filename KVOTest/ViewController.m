0510a730d59f5121c932f0dfb6f85efd056a7626
codeliu6572 <codeliu6572@163.com>
2016-09-11 09:53:57 +0800

    创建仓库

--- KVOTest/ViewController.m
+++ KVOTest/ViewController.m
@@ -8,8 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+{
+    UITableView *_tableView;
+    UIView *navView;
+}
 @end
 
 @implementation ViewController
@@ -17,6 +20,61 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    //selfNav
+    navView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 320, 64)];
+    navView.backgroundColor = [UIColor orangeColor];
+    [self.view addSubview:navView];
+    
+    
+    _tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 64, 320, 568 - 64) style:UITableViewStylePlain];
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    [self.view addSubview:_tableView];
+    //添加监听者
+
+    [_tableView addObserver:self forKeyPath:@"contentOffset" options:NSKeyValueObservingOptionNew context:nil];
+    
+    
+    for (CALayer *layer in self.view.layer.sublayers) {
+        NSLog(@"%@",layer);
+        [layer   removeFromSuperlayer];
+    }
+    
+}
+
+/**
+ *  监听属性值发生改变时回调
+ */
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    CGFloat offset = _tableView.contentOffset.y;
+    CGFloat delta = offset / 64.f + 1.f;
+    delta = MAX(0, delta);
+    navView.alpha = MIN(1, delta);
+}
+
+
+#pragma mark - UITableViewDelegate
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 44;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"%ld",indexPath.row];
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning {
